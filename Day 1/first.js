af62cac0a1a46255a17ee88c4fd07b96cd3d78a6
raghavdolyar <raghavdolyardell@gmail.com>
2026-04-26 08:03:40 +0000

    intro to node js

---
+++ Day 1/first.js
@@ -0,0 +1,11 @@
+let data = "writing in the file\nhurray!";
+
+const fs = require("fs");
+
+fs.writeFile("output.txt", data, (error) => {
+  if (error) {
+    console.log("some error occured");
+  } else {
+    console.log("file written successfully");
+  }
+});
