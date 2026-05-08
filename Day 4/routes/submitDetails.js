e1356bd67804a16f89efdab4364e6c6ecd0b7397
raghavdolyar <raghavdolyardell@gmail.com>
2026-05-08 17:52:13 +0000

    doing async I/O

--- Day 4/routes/submitDetails.js
+++ Day 4/routes/submitDetails.js
@@ -10,11 +10,13 @@ const submitDetails = (req, res) => {
 		const parsedBody = Buffer.concat(body).toString();
 		const params = new URLSearchParams(parsedBody);
 		const bodyObject = Object.fromEntries(params);
-		fs.writeFileSync('user.txt', JSON.stringify(bodyObject));
+		fs.writeFile('user.txt', JSON.stringify(bodyObject), error => {
+			console.log('File written successfully.');
+			res.statusCode = 302;
+			res.setHeader('Location', '/');
+			res.end();
+		});
 	});
-	res.statusCode = 302;
-	res.setHeader('Location', '/');
-	res.end();
 };
 
 exports.handler = submitDetails;
